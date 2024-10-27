c912b7403323eda2a70d7af4f6f30dce5ebf5a5d
mahmoud <mahmoudshalaby363@gmail.com>
2024-10-27 16:22:16 +0300

    add feat filter services

--- src/components/common/services/Popular.jsx
+++ src/components/common/services/Popular.jsx
@@ -2,14 +2,23 @@ import { Link } from "react-router-dom";
 import { Services } from "../../../data/services.jsx";
 import ServCard from "./ServCard.jsx";
 
-export default function PopularServs({ numsOfServs }) {
+export default function PopularServs({ numsOfServs, category }) {
    const servs = Services;
    return (
       <>
          <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 items-start gap-8 pb-4">
-            {servs.slice(0, numsOfServs).map((service, index) => (
-               <ServCard service={service} key={index} />
-            ))}
+            {
+               category === "allServices"
+                  ? servs.map((service, index) => (
+                     <ServCard service={service} key={index} />
+                  ))
+                  : servs
+                     .filter((el) => el.category === category)
+                     .map((service, index) => (
+                        <ServCard service={service} key={index} />
+                     ))
+            }
+
          </div>
          {numsOfServs <= 4 && <div className="flex justify-center items-center pt-8">
             <Link to='services'>
--- src/components/common/services/Services.jsx
+++ src/components/common/services/Services.jsx
@@ -1,15 +1,35 @@
-import { useEffect } from "react";
-import Categories from "../../categories/Categories.jsx";
+import { useEffect, useState } from "react";
 import PopularServs from "./Popular.jsx";
 
 export default function Services() {
+   const [categories] = useState([
+      { name: "جميع الخدمات", value: "allServices" },
+      { name: "الخدمات الأكاديمية", value: "Academic" },
+      { name: "الخدمات الرقمية", value: "Digital" },
+      { name: "خدمات التصميم والهوية", value: "Design" }
+   ]);
+   const [selectedCategory, setSelectedCategory] = useState("allServices");
+   function hanldeSelectCategory(cat) {
+      setSelectedCategory(cat.value);
+   }
+
    useEffect(() => {
       window.scrollTo(0, 0)
    }, [])
    return (
       <>
-         <Categories />
-         <PopularServs numsOfServs={12} />
+         <div className="flex gap-4 mb-6">
+            {categories.map((cat, index) => (
+               <button
+                  key={index}
+                  onClick={() => hanldeSelectCategory(cat)}
+                  className={`px-4 py-2 rounded ${selectedCategory === cat.value ? "bg-main-600 text-white" : "bg-gray-200 text-black"}`}
+               >
+                  {cat.name}
+               </button>
+            ))}
+         </div>
+         <PopularServs category={selectedCategory} />
       </>
    )
 }
