9e0e717a91d51295697d38c47651c455bc456842
mahmoud <mahmoudshalaby363@gmail.com>
2024-10-27 16:59:49 +0300

    improve click at navbar at phone at close it after click navItem

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
     <header className="bg-gray-50 flex justify-between items-center p-3 shadow-sm fixed top-0 left-0 right-0 h-[70px] z-50">
       <div className="logo">
       </div>
-      <ul className={`md:flex gap-5 items-center hidden ${toggle && '!flex flex-col gap-y-4 absolute top-[70px] left-0 right-0 w-full bg-gray-50 py-6'} `}>
+      <ul className={`md:flex gap-5 items-center hidden `}>
         <li className="text-xl font-medium" >
           <NavLink to='/'>الرئيسية</NavLink>
         </li>
@@ -24,7 +24,20 @@ export default function Header() {
         <li className="text-xl font-medium" >
           <NavLink to='/contact'>تواصل معنا</NavLink>
         </li>
-
+      </ul>
+      <ul className={`${toggle ? 'flex flex-col items-center gap-y-4 absolute top-[70px] left-0 right-0 w-full bg-gray-50 py-6' : "hidden"}`}>
+        <li className="text-xl font-medium" onClick={handlToggle}>
+          <NavLink to='/'>الرئيسية</NavLink>
+        </li>
+        <li className="text-xl font-medium" onClick={handlToggle}>
+          <NavLink to='/about-us'>من نحن</NavLink>
+        </li>
+        <li className="text-xl font-medium" onClick={handlToggle}>
+          <NavLink to='/services'>الخدمات</NavLink>
+        </li>
+        <li className="text-xl font-medium" onClick={handlToggle}>
+          <NavLink to='/contact'>تواصل معنا</NavLink>
+        </li>
       </ul>
       <div className="md:hidden flex" onClick={handlToggle}>
         <Menu className="w-10 h-10" />
